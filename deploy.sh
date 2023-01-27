3b303183b7bb3ba5d7a1dab299bfddf5122af4e4
Robert Steilberg <rsteilberg@foreflight.com>
2023-01-27 08:53:43 -0600

    fix name

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-pixlet render metar-roob/metar.star icao=KAUS,KEDC,MHRO,KSAT,KDCA && pixlet push --installation-id metar-roob "purely-veritable-relieved-duck-e1d" metar-roob/metar.webp
+pixlet render metar-roob/metar.star icao=KAUS,KEDC,MHRO,KSAT,KDCA && pixlet push --installation-id metar "purely-veritable-relieved-duck-e1d" metar-roob/metar.webp
