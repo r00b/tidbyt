be72bafc80673b5e517ffa396df9dc6afe0d819b
Robert Steilberg <rsteilberg@foreflight.com>
2023-01-28 12:19:52 -0600

    remove hack app

--- apps/metar-roob/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env zsh
-pixlet render metar-roob/metar.star icao=KAUS,KEDC,MHRO,KSAT,KDCA && pixlet push --installation-id metar "purely-veritable-relieved-duck-e1d" metar-roob/metar.webp
